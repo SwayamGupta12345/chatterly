9e0bc593aeb8edcc0da6e28409185bc8c5653f9a
Rishu Goyal <rishugoyal16800@gmail.com>
2025-12-06 14:47:21 +0530

    updating dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -39,6 +39,7 @@ export default function Dashboard() {
 
     fetchSession();
   }, []);
+
   useEffect(() => {
     const checkAuth = async () => {
       const session = await getSession();
@@ -111,6 +112,7 @@ export default function Dashboard() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-100">
+      {console.log(user)}
       {/* Mobile Sidebar Overlay */}
       {isSidebarOpen && (
         <div
